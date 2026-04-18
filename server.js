370250f13d5f2fdae0a9b3466cb188d7c268c594
82scb875n7-lab <lewissy24@gmail.com>
2026-04-17 17:08:21 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
+
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.redirect('/login.html');
+app.get("/", (req, res) => {
+  res.redirect("/login.html");
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Server running"));
+
+app.listen(PORT, () => {
+  console.log("Server running...");
+});
