16257209c44e47342fe98c4f9b493c34e650fa83
82scb875n7-lab <lewissy24@gmail.com>
2026-04-17 20:27:09 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
+// serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+// fix root route
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
 
+// VERY IMPORTANT (Render port)
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log('Server running on port ' + PORT);
+  console.log(`Server running on port ${PORT}`);
 });
